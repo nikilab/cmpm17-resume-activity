9455da3a4d638dc49e10770a5d32aeedfbe2b1cf
nikilab <bashyamnikila@gmail.com>
2026-04-22 18:14:50 -0700

    changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { ChangeDetectionStrategy, Component, signal} from '@angular/core';
+
+import {MatExpansionModule} from '@angular/material/expansion';
+
 
 @Component({
   selector: 'app-component',
-  imports: [],
+  imports: [MatExpansionModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
+  
 })
 export class AppComponent {
   title = 'cmpm17-resume-activity';
+  name: string = 'niki';
+  skills: string[] = ['python','java','naps']
+  prevexperience: string[] = ['Pricer, The Echo Shop']
+  empty: string[] = []
+  hasprevexperience(): boolean {
+    return this.prevexperience !== this.empty
+  }
+  
 }
